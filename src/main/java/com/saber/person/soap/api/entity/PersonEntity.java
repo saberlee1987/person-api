73631f352595c846fc886d2446fa37620f7b9eae
saberlee1987 <saberazizi66@yahoo.com>
2021-10-29 01:14:59 +0330

    change @data for personEntity

--- src/main/java/com/saber/person/soap/api/entity/PersonEntity.java
+++ src/main/java/com/saber/person/soap/api/entity/PersonEntity.java
@@ -1,12 +1,16 @@
 package com.saber.person.soap.api.entity;
 
-import lombok.Data;
-
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "persons",uniqueConstraints = @UniqueConstraint(name = "nationalUnique",columnNames ={"nationalCode"} ))
-@Data
+@Getter
+@Setter
+@ToString
 public class PersonEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +25,17 @@ public class PersonEntity {
     private Integer age;
     @Column(name = "email",length = 30)
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonEntity entity = (PersonEntity) o;
+        return Objects.equals(id, entity.id) && Objects.equals(firstName, entity.firstName) && Objects.equals(lastName, entity.lastName) && Objects.equals(nationalCode, entity.nationalCode) && Objects.equals(age, entity.age) && Objects.equals(email, entity.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, nationalCode, age, email);
+    }
 }
