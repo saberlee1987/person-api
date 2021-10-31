9f00cec8270e71a5541704963ac8871adadb9515
saberlee1987 <saberazizi66@yahoo.com>
2021-10-31 03:54:41 +0330

    add security for soap

--- src/main/java/com/saber/person/soap/api/config/SoapConfig.java
+++ src/main/java/com/saber/person/soap/api/config/SoapConfig.java
@@ -4,9 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.saber.person.soap.api.soap.PersonSoapRoute;
 import com.saber.person.soap.api.soap.PersonSoapService;
 import lombok.RequiredArgsConstructor;
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.bus.CXFBusFactory;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,11 +30,35 @@ public class SoapConfig {
 
     private final PersonSoapService personSoapService;
     private final ObjectMapper mapper;
+    private final CustomUsernameTokenValidator customUsernameTokenValidator;
+
     @Bean
     public PersonSoapRoute personSoapRoute(){
         PersonSoapRoute soapRoute = new PersonSoapRoute(personSoapService,mapper);
         soapRoute.setUrl(String.format("cxf:%s?serviceClass=%s",
                  endpoint,PersonSoapService.class.getName()));
         return soapRoute;
+    }
+
+    @Bean
+    public WSS4JInInterceptor wss4JInInterceptor(){
+        WSS4JInInterceptor wss4JInInterceptor = new WSS4JInInterceptor();
+        Map<String,Object> config = new HashMap<>();
+        config.put("action","UsernameToken");
+        config.put("passwordType","PasswordText");
+        wss4JInInterceptor.setProperties(config);
+        return wss4JInInterceptor;
+    }
+
+    @Bean(value = "securing-webservice")
+    public Bus cxfBus(){
+
+        BusFactory busFactory = CXFBusFactory.newInstance();
+        Bus bus = busFactory.createBus();
+        Map<String,Object> config = new HashMap<>();
+        config.put("ws-security.ut.validator",customUsernameTokenValidator);
+        bus.setProperties(config);
+        bus.getInInterceptors().add(wss4JInInterceptor());
+        return bus;
     }
 }
